3c629a2bb79591bc6dd6278b617ad090892cbe13
Jesus <jesus.cont5648@gmail.com>
2024-09-21 10:40:01 -0500

    Update Login Section

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup>
-
+  import login from "./Dentify/components/login.vue";
 </script>
 
 <template>
-  <h1>DENTIFY</h1>
+  <login></login>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/Dentify/components/login.vue
@@ -0,0 +1,73 @@
+<script>
+
+export default {
+  name: "login.vue",
+
+
+}
+</script>
+
+<template>
+
+  <div>
+    <div class="flex gap-24 items-center">
+      <pv-Image src="src/assets/img/login.png" width="80%" class="w-1/2"></pv-Image>
+      <div>
+        <div class="flex flex-col items-center justify-center">
+          <pv-Image src="src/assets/img/logoDentify.png" width="190px"></pv-Image>
+          <h1 class="mt-8 mb-8 text-center" style="font-size: 50px">Log In</h1>
+        </div>
+        <div class="flex flex-col gap-3">
+          <label for="username" class="text-left">Username</label>
+          <pv-inputtext id="username" class="custom-input" v-model="value" type="text" size="small"
+                        placeholder="enter your username"/>
+
+          <label for="password" class="text-left">Password</label>
+          <pv-inputtext type="password" id="password" class="custom-input" v-model="value" size="small" toggleMask
+                        placeholder="enter your password"/>
+
+          <pv-button class="mt-4 button">Log In</pv-button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+.custom-input {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #D1F2EB;
+  cursor: pointer;
+  transition: background-color 0.3s ease, border-color 0.3s ease;
+  color: black;
+}
+
+.button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #2C3E50;
+  cursor: pointer;
+  transition: background-color 0.3s ease, border-color 0.3s ease;
+  color: white;
+}
+
+button:hover {
+  background-color: #2C3E40;
+  color: black;
+  border-color: #2C3E50;
+
+
+}
+
+</style>
