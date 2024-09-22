f4c20bb8f1a8497ad3887a3399c66ba59cfda75d
Lucero H <120441361+lucerohh@users.noreply.github.com>
2024-09-22 14:18:49 -0500

    add profile component

---
+++ src/Dentify/components/Profile/profile-content.component.vue
@@ -0,0 +1,106 @@
+<script>
+export default {
+  name: "profile-content.component",
+}
+</script>
+
+<template>
+  <div class="profile-container">
+    <div class="title-section-container">
+      <p>Profile</p>
+    </div>
+    <div class="header-profile-container">
+      <div class="profile-logo-container">
+        <pv-Image src="src/assets/img/dental-clinic-logo.png" alt="Logo"
+                  class="profile-logo" width="128px"></pv-Image>
+      </div>
+      <div class="dental-clinic-name-container">
+        <p class="name-clinic">Lorem ipsum</p>
+        <p class="dental-clinic">Dental center</p>
+      </div>
+    </div>
+    <div class="button-edit-profile-container">
+      <pv-button class="button-edit-profile">Edit profile</pv-button>
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped>
+.profile-logo {
+  width: 20vw;
+  height: 20vw;
+  max-width: 200vw;
+  max-height: 200px;
+  object-fit: cover;
+  margin: auto;
+}
+
+.header-profile-container {
+  background-color: #D1F2EB;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  padding: 2em;
+  border-radius: 30px;
+}
+
+.profile-container {
+  width: 60vw;
+  margin: 0 auto;
+}
+
+.title-section-container {
+  font-size: 3em;
+  font-weight: bold;
+  display: flex;
+  justify-content: left;
+  margin-bottom: 0.5em;
+}
+
+.profile-logo-container {
+  margin-right: 1em;
+}
+
+.dental-clinic-name-container {
+  margin-left: 1em;
+}
+
+.name-clinic {
+  font-size: 2em;
+  font-weight: bold;
+  margin-bottom: 0.5em;
+  padding: 0;
+}
+
+.dental-clinic {
+  font-size: 1em;
+  margin-bottom: 0.5em;
+  padding: 0;
+  transform: translateY(-3em);
+}
+
+.button-edit-profile {
+  background-color: #2C3E50;
+  border: none;
+  border-radius: 30px;
+  font-size: 1em;
+  font-weight: normal;
+  padding: 0.8em 1.2em;
+  cursor: pointer;
+  transition: background-color 0.3s ease, border-color 0.3s ease;
+  color: white;
+}
+
+.button-edit-profile-container {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 1em;
+}
+
+.button-edit-profile:hover {
+  background-color: #5c738a;
+  color: white;
+}
+</style>
---
+++ src/Dentify/components/Profile/profile-general-information.component.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "profile-general-information.component"
+}
+</script>
+
+<template>
+  <div class="profile-general-information-container">
+    <p><b>General information</b></p>
+    <hr class="custom-line">
+    <p><b>Name:</b> Lorem ipsum dolor sit amet</p>
+    <p><b>Email:</b> Aliquam scelerisque viverra lobortis</p>
+  </div>
+</template>
+
+<style scoped>
+.profile-general-information-container {
+  background-color: #D1F2EB;
+  border-radius: 30px;
+  height: 20vw;
+  padding: 2em 2em;
+  text-align: left;
+  margin-top: 1em;
+  font-size: 1em;
+}
+
+.profile-general-information-container p, .profile-general-information-container hr {
+  margin: 0.7em 0.5em;
+}
+
+.custom-line {
+  background-color: black;
+  height: 0.2em;
+}
+</style>
