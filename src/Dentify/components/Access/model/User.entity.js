b1d71c9784e482dd3a6c1fb959e811bfbe2011b6
Lucero H <120441361+lucerohh@users.noreply.github.com>
2024-10-08 20:34:30 -0500

    fix profile api service

--- src/Dentify/components/Access/model/User.entity.js
+++ src/Dentify/components/Access/model/User.entity.js
@@ -1,13 +1,13 @@
 export class User {
-    constructor({user_id=0, username="", first_name="default", last_name="default",
-                    email="default", phone="default", registration_date ="default", company ="default", password = "default"}) {
-        this.id = user_id;
+    constructor({id=0, username="", first_name="default", last_name="default",
+                    email="default", phone="default", register_date ="default", company ="default", password = "default"}) {
+        this.id = id;
         this.username = username;
-        this.name = first_name;
-        this.lastname = last_name;
+        this.first_name = first_name;
+        this.last_name = last_name;
         this.email = email;
         this.phone = phone;
-        this.register_date = registration_date;
+        this.register_date = register_date;
         this.company = company;
         this.password = password;
     }
