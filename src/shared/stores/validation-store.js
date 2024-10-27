3244c3e3e16ea47638a1099b7ad2f65b50b308e9
Jesus <jesus.cont5648@gmail.com>
2024-10-27 16:53:12 -0500

    fix inventory and login feature

--- src/shared/stores/validation-store.js
+++ src/shared/stores/validation-store.js
@@ -1,15 +1,22 @@
 import { createStore } from 'vuex';
 
+// Cargar el usuario desde localStorage si existe
+const savedUser = JSON.parse(localStorage.getItem('user'));
+
 const store = createStore({
     state: {
-        user: null,
+        user: savedUser || null,
     },
     mutations: {
         setUser(state, user) {
             state.user = user;
+
+            localStorage.setItem('user', JSON.stringify(user));
         },
         clearUser(state) {
             state.user = null;
+
+            localStorage.removeItem('user');
         }
     },
     actions: {
